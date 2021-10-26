a97fd9e66849f72cf1b5216f063fd8fa473aa8f2
SvetaDash <90773701+SvetaDash@users.noreply.github.com>
2021-10-27 02:58:03 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,44 @@
 public class Main {
 
-    private static Object String;
-
     public static void main (String[] args) {
         try {
                      stringText();
         } catch (MyArraySizeException e){
             System.out.println("Размер массива неверный!");
            }catch (MyExten e){
-            System.out.println("Ошибка в ячуйке "+e.i+"*"+e.j);
+            System.out.println("Ошибка в ячeйке "+e.i+"*"+e.j+"\n");
         }
-        /* String[][] arr =  {
-                  {"hff", "bj", "hjd", "jf"},
-                  {"ghj", "shs", "hjw", "gdgs"},
-                  {"bd", "gdg", "gwg", "dfqt"},
-          };*/
+
     }
     
     public static int stringText()  throws MyArraySizeException, MyExten {
         int count = 0;
         String[][] arr = {
-                {"hff", "bj", "hjd", "jf"},
-                {"ghj", "shs", "hjw", "gdgs"},
-                {"bd", "gdg", "gwg", "dfqt"},
-                {"ghr", "dgs", "kjgf", "fet"}
+                {"3", "1", "5", "2"},
+                {"4", "9", "3", "7"},
+                {"2", "6", "1", "0"},
+                {"6", "4", "3", "7"}
         };
-        if (arr.length != 4) throw new MyArraySizeException();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length != 4)
-                throw new MyArraySizeException();
+        if (arr.length != 4) {
+            throw new MyArraySizeException();
         }
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                try {
-                    count = count + Integer.parseInt(arr[i][j]);
-                } catch (NumberFormatException e) {
-                    throw new MyExten(i, j);
-                }
-
+                Integer.parseInt(arr[i][j]);
             }
+        for (i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                    try {
+                        count = count + Integer.parseInt(arr[i][j]);
+                    } catch (NumberFormatException e) {
+                        throw new MyExten(i, j);
+                    }
+
+                }
+            }  System.out.println("Сумма всех чисел = " + count);
+
         }
+
         return count;
     }
 
